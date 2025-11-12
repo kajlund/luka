57af1b513dd52e18737341225b76036e3e538786
Kaj Lund <kaj.lund@gmail.com>
2025-11-12 19:04:49 +0200

    Up the limit

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ class App {
 
     const limiter = rateLimit({
       windowMs: 5 * 60 * 1000, // 5 minutes
-      limit: 100, // Limit each IP to 100 requests per `window` (here, per 5 minutes).
+      limit: 1000, // Limit each IP to 100 requests per `window` (here, per 5 minutes).
       standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
       legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
       // store: ... , // Redis, Memcached, etc. See below.
